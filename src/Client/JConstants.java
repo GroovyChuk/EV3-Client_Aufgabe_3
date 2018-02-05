9f9a2788a35c344cd71b54913934a7a68165940b
Kalaman <oemer.kalaman@hotmail.de>
2018-02-05 20:07:54 +0100

    Changed amount and the size of a particle

--- src/Client/JConstants.java
+++ src/Client/JConstants.java
@@ -5,14 +5,14 @@ import java.awt.*;
  * Created by Kalaman on 12.01.18.
  */
 public class JConstants {
-    public static final int WINDOW_SIZE_X = 1000;
+    public static final int WINDOW_SIZE_X = 1200;
     public static final int WINDOW_SIZE_Y = 550;
 
     public static final int PANEL_ROBOT_SIZE_X = 700;
     public static final int PANEL_CONSOLE_SIZE_X = WINDOW_SIZE_X - PANEL_ROBOT_SIZE_X;
 
     public static final int PARTICLE_AMOUNT = 1000;
-    public static final int PARTICLE_WIDTH = 6;
-    public static final int PARTICLE_HEIGHT = 6;
-    public static final int PARTICLE_DEGREE_MULTIPLICATOR = 5;
+    public static final int PARTICLE_WIDTH = 4;
+    public static final int PARTICLE_HEIGHT = 4;
+    public static final int PARTICLE_DEGREE_MULTIPLICATOR = 3;
 }
